66f5bcd427bed1e334eedd307189b39c85e5f5e1
jeagleson <jeagleson@evolenthealth.com>
2022-11-16 14:15:20 -0500

    completed lesson 3

--- App-1/app.R
+++ App-1/app.R
@@ -2,29 +2,26 @@ library(shiny)
 
 # Define UI ----
 ui <- fluidPage(
-  titlePanel(strong("Jenna's Shiny App")),
+  titlePanel(strong("censusVis")),
 
-  sidebarLayout(position = "left", # default
-    sidebarPanel(h2("Installation"),
-                 p("Shiny is available on CRAN, so you can install it in the usual way from your R console:"),
-                 code('install.packages("shiny")'),
-                 br(),
-                 br(),
-                 img(src = "rstudio.png", height = 70, width = 200),
-                 br(),
-                 "Shiny is a product of",
-                 span("Rstudio", style = "color:blue")),
-    mainPanel(
-      h1("Intoducing Shiny"),
-      p("Shiny is a new package from RStudio that makes it easy to build interactive web applications with R."),
-      br(),
-      p("For an introduction and live examples, visit the ",
-        a("Shiny homepage.",
-          href = "https://shiny.rstudio.com")),
-      h2("Features"),
-      p("- Build useful web applications with only a few lines of code - no JavaScript required"),
-      p('- Shiny applications are automatically "live" in the same way that spreadsheets are live. Outputs change instantly as users modify inputs, without requiring a reload of the browser.')
-    )
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with
+               information from the 2010 US Census."),
+
+      selectInput("var",
+                  label = "Choose a variable to display",
+                  choices = list("Percent White",
+                                 "Percent Black",
+                                 "Percent Hispanic",
+                                 "Percent Asian"),
+                  selected = "Percent White"),
+
+      sliderInput("range",
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+    mainPanel()
   )
 )
 
